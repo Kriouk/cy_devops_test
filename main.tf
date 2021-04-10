ef694196064b182a06369f76a6a0aa1d9bcf3bab
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2021-04-10 16:18:22 +0200

    Sssht, it's a secret...

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "ecr_repository_name" {}
 terraform {
   required_providers {
     aws = {
@@ -9,13 +11,13 @@ terraform {
   }
 }
 provider "aws" {
-    region = "eu-west-3"
+    region = var.aws_region
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
 }
 
 resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "testing2"
+  name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
 
   #image_scanning_configuration {
