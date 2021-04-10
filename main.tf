49e554062fc8dab093fd3916bd28596c751670ca
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2021-04-10 09:50:47 +0200

    Testing Terraform with GitHub Actions

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "eu-west-3"
+    access_key = "{{env `AWS_ACCESS_KEY`}}"
+    secret_jey = "{{env `AWS_SECRET_KEY`}}"
+}
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "testing"
+  image_tag_mutability = "MUTABLE"
+
+  #image_scanning_configuration {
+  #  scan_on_push = true
+  #}
+}
