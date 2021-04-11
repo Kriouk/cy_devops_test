a8e6b3014d0415517c2cd9e4cddec1757572970f
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2021-04-11 14:33:33 +0200

    Testing security group

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -16,3 +16,25 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 3"
+
+  name        = "testing-mysql"
+  description = "Complete MySQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+}
