b810bbb3de58b1654d79adc302b0c6b68d1333f5
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2021-04-11 11:30:09 +0200

    030

--- main.tf
+++ main.tf
@@ -2,7 +2,22 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "ecr_repository_name" {}
-
+ terraform {
+    required_providers {
+        aws = {
+          source  = "hashicorp/aws"
+          version = "~> 3.0"
+        }
+    }
+    backend "s3" {
+        bucket = "cycloid-tf-state-bucket"
+        key = "states/terraform.tfstate"
+        access_key = "${{ secrets.AWS_ACCESS_KEY }}"
+        secret_key = "${{ secrets.AWS_SECRET_KEY }}"
+        region = "${{ secrets.AWS_REGION }}"
+        encrypt = true
+    }
+}
 provider "aws" {
     region = var.aws_region
     access_key = var.aws_access_key
