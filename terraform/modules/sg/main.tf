975a6cad22a89785d5d52f13d0093b52efa91523
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2021-04-12 21:48:17 +0200

    Oops²

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -2,7 +2,7 @@ module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 3"
 
-  name        = "sg-mysql"
+  name        = "cy-test-sg-mysql"
   description = "Complete MySQL example security group"
   vpc_id      = var.vpc_id
 
